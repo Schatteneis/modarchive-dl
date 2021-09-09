51b0e0a272629c2fac76d0a24888e7b2e58590be
Schatteneis <ilovepokemonse@gmail.com>
2021-09-09 18:29:24 +0200

    added user uploaded downloads

--- modarchive-dl.sh
+++ modarchive-dl.sh
@@ -1,16 +1,47 @@
 #!/bin/bash
-userid=$1
-wget "https://modarchive.org/index.php?request=view_member_favourites_text&query=$userid" -O $userid.txt
-lines=$(echo 39)
-while [ $lines -gt 0 ]
-do
-    sed -i '$ d' $userid.txt
-    ((lines--))
+useropt='Choose Option: '
+choicemen=("User Upload" "User Favorite" "Exit")
+select opt in "${choicemen[@]}"; do
+    case $opt in
+        "User Favorite")
+            echo "User ID (query=)"
+            read userid
+            wget "https://modarchive.org/index.php?request=view_member_favourites_text&query=$userid" -O $userid.txt
+            lines=$(echo 39)
+            while [ $lines -gt 0 ]
+            do
+                sed -i '$ d' $userid.txt
+                ((lines--))
+            done
+            sed -i -e '1,179d' $userid.txt
+            for url in $(cat $userid.txt)
+            do 
+                IFS="#" read name filename <<< "$url"
+                wget $url -O $filename
+                done
+            rm $userid.txt
+            ;;
+        "User Upload")
+            echo "User ID (query=)"
+            read userid
+            wget "https://modarchive.org/index.php?request=view_artist_modules_text&query=$userid" -O $userid.txt
+            lines=$(echo 39)
+            while [ $lines -gt 0 ]
+            do
+                sed -i '$ d' $userid.txt
+                ((lines--))
+            done
+            sed -i -e '1,185d' $userid.txt
+            for url in $(cat $userid.txt)
+            do 
+                IFS="#" read name filename <<< "$url"
+                wget $url -O $filename
+                done
+            rm $userid.txt
+            ;;
+        "Exit")
+        exit
+        ;;
+        *) echo "invalid $REPLY";;
+    esac
 done
-sed -i -e '1,179d' $userid.txt
-for url in $(cat $userid.txt)
-do 
-    IFS="#" read name filename <<< "$url"
-    wget $url -O $filename
-    done
-rm $userid.txt
